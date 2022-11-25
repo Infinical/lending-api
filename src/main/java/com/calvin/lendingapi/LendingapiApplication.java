6b862d0705fe743f486e5ba78367c29b7dbbd94c
Infinical <inficalvin@gmail.com>
2022-11-25 07:27:03 +0300

    Initial commit

---
+++ src/main/java/com/calvin/lendingapi/LendingapiApplication.java
@@ -0,0 +1,16 @@
+package com.calvin.lendingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+
+@SpringBootApplication
+public class LendingapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LendingapiApplication.class, args);
+	}
+
+}
